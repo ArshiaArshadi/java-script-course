712cb02d45b562f9ecb452db7ac7a2149b726e1b
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-07-21 14:25:31 +0330

    Exercise: A School Information
    In this exercise, using the functions of the JavaScript language, I have developed the information of a school in a graphical and categorized style

---
+++ Exercise 1/school.js
@@ -0,0 +1,49 @@
+
+let school = {
+    calssA : {
+        student1 : {
+            name : 'James' ,
+            family : 'Winzer' ,
+            phone : 7744 ,
+            address : 'Oklahoma' ,
+            score : 17
+        } ,
+        student2 : {
+            name : 'Olivia' ,
+            family : 'Kennedy' ,
+            phone : 6678 ,
+            address : 'Texas' ,
+            score : 19 
+        } ,
+        student3 : {
+            name : 'Ava' ,
+            family : 'Howard' ,
+            phone : 6843 ,
+            address : 'Arizona' ,
+            score : 15 ,
+        }
+    } ,
+    classB : {
+        student1 : {
+            name : 'Mia' ,
+            family : 'Finch' ,
+            phone : 9983 ,
+            address : 'Florida' ,
+            score : 18
+        } ,
+        student2 : {
+            name : 'Emily' ,
+            family : 'Stanley' ,
+            phone : 3325 ,
+            address : 'New York	' ,
+            score : 20 
+        } ,
+        student3 : {
+            name : 'Ella' ,
+            family : 'Simpson' ,
+            phone : 4157 ,
+            address : 'California' ,
+            score : 17 ,
+        }
+    }
+}
