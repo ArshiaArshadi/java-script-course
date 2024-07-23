be1def6fc91303c6c53f4d5fa8f2dc98e23907e5
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-07-23 17:49:45 +0330

    school Information-white object and array
    Segmenting the information of a fully accessible school
    *With this partition version, you have the power to move the addition and deletion of students and even classes

---
+++ Exercise 2/school.js
@@ -0,0 +1,68 @@
+// school information
+let school = {
+    //  1 - The first level of classification
+    //      Separation of classes
+    allClasses : [
+    //      2 - Second level of classification    
+    //      Class Details
+    // *start information = calss -1-*
+        {
+    //          3 - The third level of classification     
+    //          Information about the students in the class   
+                student:[
+                {
+                    name : 'James' ,
+                    family : 'Winzer' ,
+                    phone : 7744 ,
+                    address : 'Oklahoma' ,
+                    score : 17
+                },
+                {
+                    name : 'Olivia' ,
+                    family : 'Kennedy' ,
+                    phone : 6678 ,
+                    address : 'Texas' ,
+                    score : 19 
+                } ,
+                {
+                    name : 'Ava' ,
+                    family : 'Howard' ,
+                    phone : 6843 ,
+                    address : 'Arizona' ,
+                    score : 15 ,
+                }
+            ] ,
+        },
+    // *end of information = calss -1-*
+        
+    // *start information = calss -2-*     
+        {
+                student:[ 
+                {
+                    name : 'Mia' ,
+                    family : 'Finch' ,
+                    phone : 9983 ,
+                    address : 'Florida' ,
+                    score : 18
+                } ,
+                {
+                    name : 'Emily' ,
+                    family : 'Stanley' ,
+                    phone : 3325 ,
+                    address : 'New York	' ,
+                    score : 20 
+                } ,
+                {
+                    name : 'Ella' ,
+                    family : 'Simpson' ,
+                    phone : 4157 ,
+                    address : 'California' ,
+                    score : 17 ,
+                }
+            ]
+        }       
+    // *end of information = calss -2-*
+    ]
+} 
+
+console.log(school)
