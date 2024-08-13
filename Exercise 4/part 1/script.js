505906549c80fb9d2f1b1ba7d9965c2718b3c2b1
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-08-13 18:09:57 +0330

    update 1 (sort array with for)

---
+++ Exercise 4/part 1/script.js
@@ -0,0 +1,60 @@
+// // create array
+// let numbers = [5,3,9,14,2,7];
+// // get number Count
+// let loop = numbers.length
+// // put the code in a function
+// function sortArray(array) {
+    
+//     for (let i = 1; i < loop; i++){
+        
+//         for (let index = 1; i > index; index++){
+//             if (numbers[i] < numbers[index]) {
+//                 let minimum = index
+
+//                 numbers[minimum] = numbers[i] ;
+//                 numbers[i] = numbers[minimum] ;
+//             }
+
+
+//         return numbers
+//         }
+//     }
+// }
+
+
+
+// console.log(sortArray(numbers));
+
+
+
+// create array
+let numbers = [5,3,9,14,2,7];
+// get number Count
+let loop = numbers.length
+// put the code in a function
+function sortArray(array) {
+    
+    for (let i = 1; i < loop; i++){
+    num1 =  
+        for (let index = 1; i > index; index++){
+            
+
+        return numbers
+        }
+    }
+}
+
+
+
+console.log(sortArray(numbers));
+
+
+
+
+
+
+
+
+
+
+
