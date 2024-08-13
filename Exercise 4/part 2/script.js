c0563dab118eb373ae1a5645434579e27ac31762
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-08-13 15:26:41 +0330

    stars loop
    you can get any number and give beauty stars loop

---
+++ Exercise 4/part 2/script.js
@@ -0,0 +1,16 @@
+// we need 2 loop(for)
+star = '*'
+
+// first loop ; take star (type: string) 
+for (let i = 1; i <= 3 ; i++){
+    // The number of output stars by -for-
+    console.log(star.repeat(i));
+    // with -repeat(string method)- give more stars
+}
+
+// second loop ; take star (type: string) 
+for (let i = 2; i >= 1 ; i--){
+    // The number of output stars by -for-
+    console.log(star.repeat(i));
+    // with -repeat(string method)- give more stars
+}
