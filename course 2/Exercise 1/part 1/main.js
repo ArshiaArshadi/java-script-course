e6f23b39c05f0f5a54b4dae1836a89ff9eb8411a
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-10-04 16:10:05 +0330

    main

--- course 2/Exercise 1/part 1/main.js
+++ course 2/Exercise 1/part 1/main.js
@@ -1,1 +1,18 @@
-const square = document.querySelector("square");
+const square = document.getElementById("square");
+document.addEventListener("keydown",press)
+
+
+function press(event){
+    if(event.keyCode == 49){
+        square.style.background = "red";
+        //shapeColor = square.style.background;
+    }
+// else if(event.keyCode == 50){
+//     square.style.background = "green";
+//     currentColor = square.style.background;
+// }else if(event.keyCode == 51){
+//     square.style.background = "blue";
+//     currentColor = square.style.background;
+// }
+    
+}
