63af39591c95ed8e456807bc91ac33625d821886
TcoY <arshia.arshadi1384@gmail.com>
2024-10-06 19:50:38 +0330

    Update main.js

--- course 2/Exercise 2/main.js
+++ course 2/Exercise 2/main.js
@@ -1,7 +1,7 @@
 // Date.now() + Math.floor(Math.random() * 100))
 // get DOC 
 let userId = document.querySelector("#user-id");
-let createId = document.querySelector("#create");
+
 
 
 let randomNumber = Date.now() + Math.floor(Math.random() * 100));
